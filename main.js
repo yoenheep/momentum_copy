263d7426a7c937df50bba50c7f5ead99af068f21
yoenheep <yoen3212@naver.com>
2025-02-19 16:42:43 +0900

    배경 사진, 업스케일링, 임시랜덤배경

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+  randomBG();
+
+  function randomBG() {
+    let ran = Math.floor(Math.random() * 3);
+    let bg = [`url(./img/bg0.jpg)`, `url(./img/bg1.jpg)`, `url(./img/bg2.jpg)`];
+    $("#all").css("background-image", bg[ran]);
+  }
+});
