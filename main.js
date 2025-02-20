c8204bf344266ce12177cf32bf1e063ebfd7001c
yoenheep <yoen3212@naver.com>
2025-02-20 16:54:44 +0900

    명언 슬라이드, 투두 인풋...디자인만..

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-$(document).ready(() => {
+$(document).ready(function () {
+  nowTime();
   randomBG();
 
   function randomBG() {
@@ -6,4 +7,27 @@ $(document).ready(() => {
     let bg = [`url(./img/bg0.jpg)`, `url(./img/bg1.jpg)`, `url(./img/bg2.jpg)`];
     $("#all").css("background-image", bg[ran]);
   }
+
+  // 시간 가져오기
+  function nowTime() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = ("0" + now.getMinutes()).slice(-2);
+    $("#currentTime").text(`${hour}:${minute}`);
+  }
+  setInterval(() => {
+    nowTime();
+  }, 1000);
+
+  // centerBottom hover시
+  $("#centerBottom").on({
+    mouseenter: function () {
+      console.log("인");
+      $("#CBman").slideDown(500);
+    },
+    mouseleave: function () {
+      console.log("아웃");
+      $("#CBman").slideUp(500);
+    },
+  });
 });
